6598ac36de052ad6fd7bd5a03a3e79aff4bb3bb4
Venkata Bhattaram <tinitiate@users.noreply.github.com>
2019-12-20 11:49:22 -0500

    1

--- 003-python-modules/019-python-logger.py
+++ 003-python-modules/019-python-logger.py
@@ -1,76 +1,90 @@
-#========================================================================================
-# TOPIC: PYTHON - SENDING EMAIL
-#========================================================================================
-# NOTES: * PYTHON provides a standard logging module to log messages
-#        * LOG to Console
-#        * LOG to File
-#        
-#========================================================================================
-#
-# FILE-NAME       : 036_python_logger.py
-# DEPENDANT-FILES : N/A 
-# 
-# AUTHOR          : tinitiate.com / Venkata Bhattaram
-#                   (c) 2014
-#
-# DESC            : PYTHON LOGGING
-#
-#========================================================================================
+""" MARKDOWN
+---
+YamlDesc: CONTENT-ARTICLE
+Title: Python Modules Logging
+MetaDescription: Python Modules, Logging, log to console, log to file
+MetaKeywords: Python Modules, Logging, log to console, log to file
+Author: Venkata Bhattaram / tinitiate.com
+ContentName: python-modules-logging
+---
+MARKDOWN """
+
+""" MARKDOWN
+# Python Logging
+* Python provides a standard logging module to log messages
+* Here we demonstrate the following 
+  * Setting Log Levels
+  * LOG to Console 
+  * LOG to File
+MARKDOWN """
+# MARKDOWN ```
 
 import sys, logging
 
-##########################
-# 1) LOG to CONSOLE
-##########################
+################################################################################
+# LOGGING MODULE - Log to console
+################################################################################
 
-# Create a LOGGING Object
-tinitiateLOG = logging.getLogger('tinitiate')
-tinitiateLOG.setLevel(logging.DEBUG)
-print(tinitiateLOG.getEffectiveLevel())
+# STEP 1. Create a LOGGING Object
+tiConsLog = logging.getLogger('tinitiate-console')
 
+# STEP 2. Create a Streamhandler for the LOGGING Object
+ConsHandler = logging.StreamHandler( sys.__stdout__ )
 
-# Create a Streamhandler for console
-tinLOG = logging.StreamHandler( sys.__stdout__ )
+# STEP 3. Set Log Formatting
+formatter = logging.Formatter('[%(asctime)s] %(name)-12s: %(levelname)-8s %(message)s')
+ConsHandler.setFormatter(formatter)
 
+# STEP 4. Add the Console Handler to the LOGGING Object
+tiConsLog.addHandler(ConsHandler)
 
-# Print to console level, Add the stream handler to Console
-tinLOG.setLevel(logging.DEBUG)
-tinitiateLOG.addHandler(tinLOG)
+# STEP 5. Set Log Level
+tiConsLog.setLevel(logging.DEBUG)
+# print(tiConsLog.getEffectiveLevel())
 
 
 # Use the functions [debug, info, warn, error] of the logging object
-tinitiateLOG.debug('This is a debug message')
-tinitiateLOG.info('This is a info message')
-tinitiateLOG.warn('This is a warning message')
-tinitiateLOG.error('This is an error message')
-tinitiateLOG.critical('This is a critical message')
+tiConsLog.debug('This is a debug message')
+tiConsLog.info('This is a info message')
+tiConsLog.warn('This is a warning message')
+tiConsLog.error('This is an error message')
+tiConsLog.critical('This is a critical message')
 
-##########################
-# 2) LOG to FILE
-##########################
+# Change Log level and retry logging all level log messages
+tiConsLog.setLevel(logging.ERROR)
 
-# Create a File Handler
-LogFile = logging.FileHandler('c:\\tinitiate\\tinPython.log')
+tiConsLog.debug('This is a debug message')
+tiConsLog.info('This is a info message')
+tiConsLog.warn('This is a warning message')
+tiConsLog.error('This is an error message')
+tiConsLog.critical('This is a critical message')
 
-# Set Log level of ERROR, ONLY ERROR or HIGHER are logged to FILE
-LogFile.setLevel(logging.ERROR)
 
-# Add the handler to the logging object
-tinitiateLOG.addHandler(LogFile)
+################################################################################
+# LOGGING MODULE - Log to file
+################################################################################
+
+# STEP 1. Create a LOGGING Object
+tiFileLog = logging.getLogger('tinitiate-file')
+
+# STEP 2. Create a Log File Handler, the log messages will be created in 
+#         this file
+LogFile = logging.FileHandler('c:\\personal\\tinitiate\\tinPython.log')
+
+# STEP 3. Add the handler to the logging object
+tiFileLog.addHandler(LogFile)
+
+# STEP 4. Set Log Level
+tiFileLog.setLevel(logging.ERROR)
+
 
 # Run Log messages
-tinitiateLOG.debug('This is a debug message')
-tinitiateLOG.info('This is a info message')
-tinitiateLOG.warn('This is a warning message')
+tiFileLog.debug('This is a debug message')
+tiFileLog.info('This is a info message')
+tiFileLog.warn('This is a warning message')
 
 # ONLY THE FOLLOEING MESSAGES ARE LOGGED TO FILE
-tinitiateLOG.error('This is an error message')
-tinitiateLOG.critical('This is a critical message')
+tiFileLog.error('This is an error message')
+tiFileLog.critical('This is a critical message')
 
-
-#========================================================================================
-# END OF CODE
-#========================================================================================
-#TAGS: PYTHON - LOGGING  LOG DEBUG LOG ERROR LOG WARNING LOG ERROR LOG CRITICAL
-#               LOG TO CONSOLE LOG TO FILE
-#========================================================================================
+# MARKDOWN ```
