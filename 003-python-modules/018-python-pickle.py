b032e48734d2e138a360cf1aae4a93a604769eb8
Venkata Bhattaram <tinitiate@users.noreply.github.com>
2019-12-20 11:10:16 -0500

    1

--- 003-python-modules/018-python-pickle.py
+++ 003-python-modules/018-python-pickle.py
@@ -1,16 +1,27 @@
-# Title: Python Pickle Serialization
+""" MARKDOWN
+---
+YamlDesc: CONTENT-ARTICLE
+Title: Python Pickle Serialization and De-Serialization
+MetaDescription: Python Pickle Serialization and De-Serialization
+MetaKeywords: Python Pickle Serialization and De-Serialization
+Author: Venkata Bhattaram / tinitiate.com
+ContentName: python-pickle-serialization
+---
+MARKDOWN """
 
-# Notes:
-# Serialization is the process of converting an object state 
-# into a string format file or memory buffer,
-# In order to resurrected for later use for a later use or 
-# transmitted across a network
+""" MARKDOWN
+# Python Pickle Serialization and De-Serialization
+* Serialization is the process of converting an object state into a 
+  binary file, this file can be stored on filesystem or transmitted across
+  network. or it can be persisted(stored) and later use. 
+* Serialization of an object is also known as deflating or marshalling.
+* In Python we use the **PICKLE** module to Serialize an Object.
+* In order to resurrect an Object from a Pickle file it needs to be deserialized
+* Deserialization of a file into an object also known as inflating or 
+  unmarshalling.
+MARKDOWN """
 
-# serializing an object is also known as deflating or marshalling
-# deserialization a string or file into an object also known as inflating or unmarshalling
-
-
-
+# MARKDOWN ```
 
 import pickle;
 
@@ -33,11 +44,11 @@ class PickleTest():
 Obj1 = PickleTest(1,2)
 
 # Serialize an Object
-with open('object.pickle', 'wb') as f:
+with open('c:\\Personal\\tinitiate\\object.pickle', 'wb') as f:
 	pickle.dump(Obj1, f)
     
 # DeSerialize File to Object
-with open('object.pickle', 'rb') as f:
+with open('c:\\Personal\\tinitiate\\object.pickle', 'rb') as f:
 	ObjFile = pickle.load(f)
 
 print(ObjFile)
@@ -49,19 +60,18 @@ print(ObjFile.b)
 ################################
 # Serialization of a Dictionary
 ################################
-   
+
 # Create a Dict
 Dict1={'APPLE':'FRUIT', 'POTATO':'ROOT', 'OKRA':'VEGETABLE'}
 
 # Serialize an Dict
-with open('dict.pickle', 'wb') as f:
+with open('c:\\Personal\\tinitiate\\dict.pickle', 'wb') as f:
 	pickle.dump(Dict1, f)
 
 # DeSerialize File to Object
-with open('dict.pickle', 'rb') as f:
+with open('c:\\Personal\\tinitiate\\dict.pickle', 'rb') as f:
 	DictFile = pickle.load(f)
     
 print(DictFile)
 
-
-    
+# MARKDOWN ```
